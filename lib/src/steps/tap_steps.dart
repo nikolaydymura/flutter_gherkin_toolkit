e4f7c09bf4aafc781e044acdb718a55c1e8ca356
Nikolay Dymura <nikolay.dymura@gmail.com>
2023-06-24 09:56:13 +0300

    Added tap samples

--- lib/src/steps/tap_steps.dart
+++ lib/src/steps/tap_steps.dart
@@ -1,24 +1,6 @@
-// tap on text `Increment` #1
-// tap on text `Increment`->first #2
-// tap on text `Increment`->last
-// tap on text `Increment`->2
-
-
-// tap on text TextButton->`Increment` #2
-// tap on text FloatingActionButton->`Increment`
-
-// tap on text FloatingActionButton->`Increment`->first #3
-
-
-// tap on type FloatingActionButton
-// tap on icon `Icons.add`
-// tap on image `assets/images/arrow_back.png`
-
 import 'package:gherkin/gherkin.dart';
 
 import '../worlds/widget_tester_world.dart';
-
-// with 1 arg
 
 class TapOnTextStep
     extends Then3WithWorld<String?, String, String?, WidgetTesterWorld> {
@@ -32,9 +14,66 @@ class TapOnTextStep
   }
 
   @override
-  Pattern get pattern => RegExp(r'tap on text (?:(.*)'
+  Pattern get pattern => RegExp(r'tap on text (?:([A-Z][A-Za-z0-9$]+)'
       '$separator'
-      ')?`(.*)`(?:'
+      r')?`(.*)`(?:'
+      '$separator'
+      r'(first|last|\d+))?');
+}
+
+class TapOnWidgetStep
+    extends Then3WithWorld<String?, String, String?, WidgetTesterWorld> {
+  final String separator;
+
+  TapOnWidgetStep({this.separator = '->'});
+
+  @override
+  Future<void> executeStep(String? input1, String input2, String? input3) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Pattern get pattern => RegExp(r'tap on widget (?:([A-Z][A-Za-z0-9$]+)'
+      '$separator'
+      r')?([A-Z][A-Za-z0-9$]+)(?:'
+      '$separator'
+      r'(first|last|\d+))?');
+}
+
+class TapOnIconsStep
+    extends Then3WithWorld<String?, String, String?, WidgetTesterWorld> {
+  final String separator;
+
+  TapOnIconsStep({this.separator = '->'});
+
+  @override
+  Future<void> executeStep(String? input1, String input2, String? input3) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Pattern get pattern => RegExp(r'tap on icon (?:([A-Z][A-Za-z0-9$]+)'
+      '$separator'
+      r')?`((?:Icons|FontAwesomeIcons)\..*)`(?:'
+      '$separator'
+      r'(first|last|\d+))?');
+}
+
+class TapOnImageStep
+    extends Then3WithWorld<String?, String, String?, WidgetTesterWorld> {
+  final String separator;
+
+  TapOnImageStep({this.separator = '->'});
+
+  @override
+  Future<void> executeStep(String? input1, String input2, String? input3) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Pattern get pattern => RegExp(r'tap on image (?:([A-Z][A-Za-z0-9$]+)'
+      '$separator'
+      r')?`(.*\.(?:png|jpg|svg|jpeg))`(?:'
       '$separator'
       r'(first|last|\d+))?');
 }
--- test/tap_test.dart
+++ test/tap_test.dart
@@ -2,12 +2,241 @@ import 'package:flutter_gherkin_toolkit/src/steps/tap_steps.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-
-  test('tap on text hello', (){
+  test('tap on text hello', () {
     final step = TapOnTextStep();
     const input = 'tap on text `hello`';
     final matches = step.pattern.allMatches(input);
     expect(matches.length, 1);
     expect(matches.first.group(2), 'hello');
+  });
+
+  test('tap on text hello last', () {
+    final step = TapOnTextStep();
+    const input1 = 'tap on text `hello`->last';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(matches.first.group(2), 'hello');
+    expect(matches.first.group(3), 'last');
+  });
+
+  test('tap on text hello first', () {
+    final step = TapOnTextStep();
+    const input1 = 'tap on text `hello`->first';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(matches.first.group(2), 'hello');
+    expect(matches.first.group(3), 'first');
+  });
+
+  test('tap on text hello 12', () {
+    final step = TapOnTextStep();
+    const input1 = 'tap on text `hello`->12';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(matches.first.group(2), 'hello');
+    expect(matches.first.group(3), '12');
+  });
+
+  test('tap on text hello in TextButton', () {
+    final step = TapOnTextStep();
+    const input1 = 'tap on text TextButton->`hello`';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(matches.first.group(1), 'TextButton');
+    expect(matches.first.group(2), 'hello');
+  });
+
+  test('tap on text hello in TextButton at 4', () {
+    final step = TapOnTextStep();
+    const input1 = 'tap on text TextButton->`hello`->4';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(matches.first.group(1), 'TextButton');
+    expect(matches.first.group(2), 'hello');
+    expect(matches.first.group(3), '4');
+  });
+
+  test('tap on icon Icons.add', () {
+    final step = TapOnIconsStep();
+    const input = 'tap on icon `Icons.add`';
+    final matches = step.pattern.allMatches(input);
+    expect(matches.length, 1);
+    expect(matches.first.group(2), 'Icons.add');
+  });
+
+  test('tap on icon Icons.add 12', () {
+    final step = TapOnIconsStep();
+    const input1 = 'tap on icon `Icons.add`->12';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(matches.first.group(2), 'Icons.add');
+    expect(matches.first.group(3), '12');
+  });
+
+  test('tap on icon Icons.add last', () {
+    final step = TapOnIconsStep();
+    const input1 = 'tap on icon `Icons.add`->last';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(matches.first.group(2), 'Icons.add');
+    expect(matches.first.group(3), 'last');
+  });
+
+  test('tap on icon Icons.add first', () {
+    final step = TapOnIconsStep();
+    const input1 = 'tap on icon `Icons.add`->first';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(matches.first.group(2), 'Icons.add');
+    expect(matches.first.group(3), 'first');
+  });
+
+  test('tap on icon Icons.add in TextButton', () {
+    final step = TapOnIconsStep();
+    const input1 = 'tap on icon TextButton->`Icons.add`';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(matches.first.group(1), 'TextButton');
+    expect(matches.first.group(2), 'Icons.add');
+  });
+
+  test('tap on on icon Icons.add in TextButton at 4', () {
+    final step = TapOnIconsStep();
+    const input1 = 'tap on icon TextButton->`Icons.add`->4';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(matches.first.group(1), 'TextButton');
+    expect(matches.first.group(2), 'Icons.add');
+    expect(matches.first.group(3), '4');
+  });
+
+  test('tap on on icon Icons.add in TextButton first', () {
+    final step = TapOnIconsStep();
+    const input1 = 'tap on icon TextButton->`Icons.add`->first';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(matches.first.group(1), 'TextButton');
+    expect(matches.first.group(2), 'Icons.add');
+    expect(matches.first.group(3), 'first');
+  });
+
+  test('tap on on icon Icons.add in TextButton last', () {
+    final step = TapOnIconsStep();
+    const input1 = 'tap on icon TextButton->`Icons.add`->last';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(matches.first.group(1), 'TextButton');
+    expect(matches.first.group(2), 'Icons.add');
+    expect(matches.first.group(3), 'last');
+  });
+
+  test('tap on widget TextButton', () {
+    final step = TapOnWidgetStep();
+    const input = 'tap on widget TextButton';
+    final matches = step.pattern.allMatches(input);
+    expect(matches.length, 1);
+    expect(matches.first.group(2), 'TextButton');
+  });
+
+  test('tap on widget TextButton 12', () {
+    final step = TapOnWidgetStep();
+    const input1 = 'tap on widget TextButton->12';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(matches.first.group(2), 'TextButton');
+    expect(matches.first.group(3), '12');
+  });
+
+  test('tap on widget TextButton first', () {
+    final step = TapOnWidgetStep();
+    const input1 = 'tap on widget TextButton->first';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(matches.first.group(2), 'TextButton');
+    expect(matches.first.group(3), 'first');
+  });
+
+  test('tap on widget TextButton in Container', () {
+    final step = TapOnWidgetStep();
+    const input1 = 'tap on widget Container->TextButton';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(matches.first.group(1), 'Container');
+    expect(matches.first.group(2), 'TextButton');
+  });
+
+  test('tap on widget TextButton in Container first', () {
+    final step = TapOnWidgetStep();
+    const input1 = 'tap on widget Container->TextButton->first';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(matches.first.group(1), 'Container');
+    expect(matches.first.group(2), 'TextButton');
+    expect(matches.first.group(3), 'first');
+  });
+
+  test('tap on image "assets/images/arrow_back.svg"', () {
+    final step = TapOnImageStep();
+    const input = 'tap on image `assets/images/arrow_back.svg`';
+    final matches = step.pattern.allMatches(input);
+    expect(matches.length, 1);
+    expect(
+        matches.first.group(2), 'assets/images/arrow_back.svg');
+  });
+
+  test('tap on image "assets/images/arrow_back.svg" last', () {
+    final step = TapOnImageStep();
+    const input1 =
+        'tap on image `assets/images/arrow_back.svg`->last';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(
+        matches.first.group(2), 'assets/images/arrow_back.svg');
+    expect(matches.first.group(3), 'last');
+  });
+
+  test('tap on image "assets/images/arrow_back.svg" first', () {
+    final step = TapOnImageStep();
+    const input1 =
+        'tap on image `assets/images/arrow_back.svg`->first';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(
+        matches.first.group(2), 'assets/images/arrow_back.svg');
+    expect(matches.first.group(3), 'first');
+  });
+
+  test('tap on image "assets/images/arrow_back.svg" 8', () {
+    final step = TapOnImageStep();
+    const input1 =
+        'tap on image `assets/images/arrow_back.svg`->8';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(
+        matches.first.group(2), 'assets/images/arrow_back.svg');
+    expect(matches.first.group(3), '8');
+  });
+
+  test('tap on image "assets/images/arrow_back.svg" in TextButton', () {
+    final step = TapOnImageStep();
+    const input1 =
+        'tap on image TextButton->`assets/images/arrow_back.svg`';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(matches.first.group(1), 'TextButton');
+    expect(
+        matches.first.group(2), 'assets/images/arrow_back.svg');
+  });
+
+  test('tap on image "assets/images/arrow_back.svg" in TextButton 4', () {
+    final step = TapOnImageStep();
+    const input1 =
+        'tap on image TextButton->`assets/images/arrow_back.svg`->4';
+    final matches = step.pattern.allMatches(input1);
+    expect(matches.length, 1);
+    expect(matches.first.group(1), 'TextButton');
+    expect(
+        matches.first.group(2), 'assets/images/arrow_back.svg');
+    expect(matches.first.group(3), '4');
   });
 }
