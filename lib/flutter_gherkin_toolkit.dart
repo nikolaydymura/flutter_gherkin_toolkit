4840965dabe05187a3566fd0d0597ae9b0615e11
Nikolay Dymura <nikolay.dymura@gmail.com>
2023-09-13 16:32:46 +0300

    Fixed analyze

--- lib/flutter_gherkin_toolkit.dart
+++ lib/flutter_gherkin_toolkit.dart
@@ -8,7 +8,6 @@ export 'src/steps/tap_steps.dart';
 export 'src/steps/load_widget_step.dart';
 export 'src/worlds/widget_tester_world.dart';
 export 'src/parameters/bloc_parameter.dart';
-export 'src/parameters/event_parameter.dart';
 export 'src/widget_runner.dart';
 export 'src/test_scenario_widgets.dart';
 export 'src/steps/tap_on_text_step.dart';
